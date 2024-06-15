965929318459d9e40e92f2f1a32637742768eadb
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-15 16:49:35 -0300

    Adicionando pratica03 e 04

---
+++ Praticas-Video-Aula/Panda-Data-Frames/pratica-04.py
@@ -0,0 +1,18 @@
+# Autor: Gabriel do Amaral Alves
+# Curso: Satander Coders - Data Science
+# Data: 15/06/2024
+# Aula: Panda DataFrames
+
+import pandas as pd
+# Criando um dicionario com autores, precos e titulos
+dicionario = {"Autores": ["Rick Riordan", "J. R. R. Tolkiem", "Rick Riordan", "Machado de Assis"],
+              "Titulos": ["O Ladrão de Raios", "A Sociedade do Anel", "Mar de Monstros", "Memórias Postumas de Brás Cubas"],
+              "Preços": [41.2, 35.7, 39.5, 40.5]}
+
+# Data Frame
+df = pd.DataFrame(dicionario)
+
+df = pd.concat([df, pd.DataFrame(
+    [{'Autores': 'Rick Riordan', 'Titulos': 'A Maldição do Titã', 'Preços': 40.20}])], ignore_index=True)
+
+print(df)
