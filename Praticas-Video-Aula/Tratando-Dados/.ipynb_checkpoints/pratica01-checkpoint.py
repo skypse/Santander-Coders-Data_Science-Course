796e89e43af662ab1cf531c6301e9d357750ce5e
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-15 18:42:10 -0300

    adicionando consulta de dados a um data set .csv

---
+++ Praticas-Video-Aula/Tratando-Dados/.ipynb_checkpoints/pratica01-checkpoint.py
@@ -0,0 +1,9 @@
+# Autor: Gabriel do Amaral Alves
+# Curso: Satander Coders - Data Science
+# Data: 15/06/2024
+# Aula: Tratando dados
+
+import pandas as pd
+
+peoples = pd.read_csv("./people-100.csv")
+peoples.head()
