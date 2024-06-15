2fac675a48cc87289047ce722aea0bbebedf9efa
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-15 13:08:24 -0300

    Adicionando pratica-04

---
+++ Praticas-Video-Aula/Introducao-Numpy/pratica-04.py
@@ -0,0 +1,10 @@
+# Autor: Gabriel do Amaral Alves
+# Curso: Satander Coders - Data Science
+# Data: 15/06/2024
+# Aula: Introdução ao Numpy
+
+import numpy as np
+
+# Matriz com Numpy
+matriz_zeros = np.zeros((5, 3))
+print(matriz_zeros)
