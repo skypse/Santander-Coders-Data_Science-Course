d2e52a9f8ac748b22771314c3fb7c315d89ed8ec
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-15 16:26:18 -0300

    Adicionando pratica-02

---
+++ Praticas-Video-Aula/Panda-Data-Frames/pratica-02.py
@@ -0,0 +1,21 @@
+# Autor: Gabriel do Amaral Alves
+# Curso: Satander Coders - Data Science
+# Data: 15/06/2024
+# Aula: Panda DataFrames
+
+import pandas as pd
+
+# Criando um dicionario com autores, precos e titulos
+dicionario = {"Autores": ["Rick Riordan", "J. R. R. Tolkiem", "Rick Riordan", "Machado de Assis"],
+              "Titulos": ["O Ladrão de Raios", "A Sociedade do Anel", "Mar de Monstros", "Memórias Postumas de Brás Cubas"],
+              "Preços": [41.2, 35.7, 39.5, 40.5]}
+
+# Data Frame
+df = pd.DataFrame(dicionario)
+print("-------------------------------------")
+print("data frame somente com os autores:")
+print(df['Autores'])
+print("-------------------------------------")
+print("data frame puxando somente 1 autor:")
+print(df['Autores'][1])
+print("-------------------------------------")
