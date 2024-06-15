c95dd1707eb997b1688c3e3c9e31eb2c297d0e69
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-15 18:53:50 -0300

    Resolvido divergência e deletados arquivos desnecessários

--- Praticas-Video-Aula/Tratando-Dados/.ipynb_checkpoints/pratica01-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# Autor: Gabriel do Amaral Alves
-# Curso: Satander Coders - Data Science
-# Data: 15/06/2024
-# Aula: Tratando dados
-
-import pandas as pd
-
-peoples = pd.read_csv("./people-100.csv")
-peoples.head()
